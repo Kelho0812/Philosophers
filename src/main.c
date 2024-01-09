691d0380568d316d691413e80b1fab83e74604ee
Kelho <Kelho88@pm.me>
2024-01-09 16:52:28 +0000

    refactored the whole logic

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: jorteixe <jorteixe@student.42porto.fr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/04 10:06:06 by jorteixe          #+#    #+#             */
-/*   Updated: 2024/01/08 10:53:16 by jorteixe         ###   ########.fr       */
+/*   Updated: 2024/01/09 16:32:07 by jorteixe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,14 @@ int	main(int argc, char **argv)
 {
 	t_data	data;
 
-	if (argc != 5 && argc != 6)
-		error_handler(WRONG_ARG, NULL, NULL);
-	initializer(&data, argv);
-	threads_create(&data);
-	while (data.finish != true)
-		monitor(&data);
-	threads_join(&data);
-	ft_exit(&data);
-	return (0);
+	if (argc == 5 && argc == 6)
+	{
+		if (!initializer(&data, argv, argc))
+			return(1);
+		dinner(&data);
+		ft_exit(&data);
+	}
+	else
+		return(error_handler(WRONG_ARG_NR));
+	return (0);	
 }
