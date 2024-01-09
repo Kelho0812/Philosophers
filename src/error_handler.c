5b60925beb19f4cb8f05cb53779f484e61db5d13
Kelho <Kelho88@pm.me>
2024-01-09 16:49:19 +0000

    changed error handling to remove exit

--- src/error_handler.c
+++ src/error_handler.c
@@ -6,42 +6,41 @@
 /*   By: jorteixe <jorteixe@student.42porto.fr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/04 10:06:03 by jorteixe          #+#    #+#             */
-/*   Updated: 2024/01/08 15:05:03 by jorteixe         ###   ########.fr       */
+/*   Updated: 2024/01/09 16:40:06 by jorteixe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/philosophers.h"
 
-int	error_handler(int error, void *param, void **param2)
+int	error_handler(t_errorcode error)
 {
-	(void)param;
-	(void)param2;
-	if (error == WRONG_ARG)
+	if (error == WRONG_ARG_NR)
 	{
-		printf(RED "Wrong num of Arguments\n" RESET);
-		printf("Please write: " CYN "./philo " RESET);
-		printf(GRN "<number_of_philosophers> <time_to_die>");
-		printf("<time_to_eat> <time_to_sleep> ");
-		printf("[number_of_times_each_philosopher_must_eat](optional)\n" RESET);
+		printf(RED "Error: Wrong number of arguments\n");
+		printf(GRN "Try ./philo 4 410 200 200 [5]\n" RESET);
 	}
 	if (error == WRONG_CHARS)
-		printf("Arguments must be " RED "only " GRN "numbers\n" RESET);
-	if (error == INVALID_TIME)
-		printf("Arguments " RED "can't be " RESET "negative or 0\n");
-	if (error == PHIL_MALLOC)
-		printf("Error Phil Malloc\n");
-	exit(1);
+	{
+		printf(RED "Error: Arguments are not numbers\n");
+		printf("Make sure not to use negatives or spaces\n");
+		printf(GRN "Try ./philo 4 410 200 200 [5]\n" RESET);
+	}
+	if (error == MALLOC_PHILO)
+		printf(RED "Error: Philosophers Malloc Invalid\n" RESET);
+	if (error == MALLOC_FORKS)
+		printf(RED "Error: Forks Malloc Invalid\n" RESET);
+	return (0);
 }
 
-void	ft_exit(t_data *data)
-{
-    int	i;
+// void	ft_exit(t_data *data)
+// {
+// 	int	i;
 
-    i = 0;
-    while (i < data->n_phil)
-    {
-        pthread_mutex_destroy(&data->philos[i].right_fork);
-        i++;
-    }
-    free((*data).philos);
-}
+// 	i = 0;
+// 	while (i < data->nbr_philos)
+// 	{
+// 		pthread_mutex_destroy(&philos[i].right_fork);
+// 		i++;
+// 	}
+// 	free((*data).philos);
+// }
