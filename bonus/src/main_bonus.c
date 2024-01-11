0c4a18affede5178bd0a59a6bbda160dd3023927
Kelho <Kelho88@pm.me>
2024-01-11 15:52:18 +0000

    updated main logic

--- bonus/src/main_bonus.c
+++ bonus/src/main_bonus.c
@@ -1,7 +1,29 @@
-# include "../include/philosophers_bonus.h"
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main_bonus.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jorteixe  <jorteixe@student.42porto.>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/10 14:51:29 by jorteixe          #+#    #+#             */
+/*   Updated: 2024/01/11 11:16:12 by jorteixe         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-int main(void)
+#include "../include/philosophers_bonus.h"
+
+int	main(int argc, char **argv)
 {
-	printf("Teste");
-	return 0;
+	t_data	data;
+
+	if (argc == 5 || argc == 6)
+	{
+		if (!initializer(&data, argc, argv))
+			return(1);
+		dinner(&data);
+		ft_exit(&data);
+	}
+	else
+		return(error_handler(WRONG_ARG_NR));
+	return (0);
 }
