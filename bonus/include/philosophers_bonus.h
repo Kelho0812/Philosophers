c943bd8a705c0fc6850e44e68979497af0eec124
Kelho <Kelho88@pm.me>
2024-01-11 10:04:14 +0000

    first bonus commit

---
+++ bonus/include/philosophers_bonus.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philosophers_bonus.h                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jorteixe  <jorteixe@student.42porto.>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/10 14:44:47 by jorteixe          #+#    #+#             */
+/*   Updated: 2024/01/10 14:44:47 by jorteixe         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILOSOPHERS_BONUS_H
+#define PHILOSOPHERS_BONUS_H
+
+# include <pthread.h>
+# include <stdbool.h>
+# include <stdint.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <sys/time.h>
+# include <unistd.h>
+
+#endif
---
+++ bonus/src/main_bonus.c
@@ -0,0 +1,7 @@
+# include "../include/philosophers_bonus.h"
+
+int main(void)
+{
+	printf("Teste");
+	return 0;
+}
